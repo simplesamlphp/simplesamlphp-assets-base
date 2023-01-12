f1cd398cd64a6bc1c9942408e2e31ade8528bee8
Tim van Dijen <tvdijen@gmail.com>
2023-01-12 10:28:46 +0100

    Fix paths

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const buildDir = __dirname + '/base/';
+const buildDir = __dirname + '/';
 
 const localConfig = {
     css_filename: 'css/[name].css',
